3c688fdf14693c6c9981f3a809b52838ad36a055
adisti.putri <faaay.adistiii@gmail.com>
2025-05-05 16:05:32 +0700

    feat: ajax getdatas

--- NGKBusi/Areas/Purchasing/Controllers/WorkingOrderController.cs
+++ NGKBusi/Areas/Purchasing/Controllers/WorkingOrderController.cs
@@ -4,16 +4,38 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using NGKBusi.Models;
+using Microsoft.AspNet.Identity;
+using NGKBusi.Areas.Purchasing.Models;
 
 namespace NGKBusi.Areas.Purchasing.Controllers
 {
     public class WorkingOrderController : Controller
     {
         DefaultConnection db = new DefaultConnection();
+        WOConnection dbm = new WOConnection();
 
         public ActionResult Index()
         {
+            var currUserId = User.Identity.GetUserId();
+            ViewBag.currUsr = currUserId;
+
             return View();
+        }
+
+        public JsonResult GetDatas()
+        {
+            var datas = dbm.FA_WO
+                .Select(data => new
+                {
+                    data.ID,
+                    data.Date,
+                    data.Number,
+                    data.Vendor,
+                    data.Subject,
+                    data.NIK
+                }).ToList();
+
+            return Json(new { datas = datas }, JsonRequestBehavior.AllowGet);
         }
     }
 }
--- NGKBusi/Areas/Purchasing/Models/WorkingOrder.cs
+++ NGKBusi/Areas/Purchasing/Models/WorkingOrder.cs
@@ -2,10 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
 
 namespace NGKBusi.Areas.Purchasing.Models
 {
     public class WorkingOrder
     {
+    }
+
+    public class FA_WO
+    {
+        [Key]
+        public int ID { get; set; }
+        public DateTime Date { get; set; }
+        public string Number { get; set; }
+        public string Vendor { get; set; }
+        public string Subject { get; set; }
+        public string NIK { get; set; }
+        public DateTime Timestamps { get; set; }
+    }
+
+    public class WOConnection : DbContext
+    {
+        public DbSet<FA_WO> FA_WO { get; set; }
+        public WOConnection()
+        {
+            this.Database.Connection.ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        }
     }
 }
