435d5a25ddcdf090fa0a7ab222bd11abc9f3ced1
adisti.putri <faaay.adistiii@gmail.com>
2025-05-05 10:25:44 +0700

    FIRST INIT WORKING ORDER PURCHASING

---
+++ NGKBusi/Areas/Purchasing/Controllers/WorkingOrderController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NGKBusi.Models;
+
+namespace NGKBusi.Areas.Purchasing.Controllers
+{
+    public class WorkingOrderController : Controller
+    {
+        DefaultConnection db = new DefaultConnection();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ NGKBusi/Areas/Purchasing/Models/WorkingOrder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NGKBusi.Areas.Purchasing.Models
+{
+    public class WorkingOrder
+    {
+    }
+}
